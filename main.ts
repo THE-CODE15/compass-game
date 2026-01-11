3c39cb1066b0d7b6d3bcd74c06fb43a587a64fb9
THE-CODE15 <kotechad@york-house.com>
2026-01-11 14:19:58 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.compassHeading() < 45 || input.compassHeading() > 315) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
